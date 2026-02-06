540ca5cb3a44f38cc1e272c53a8fdcb5e821d0d2
Thisari Hettiarachchi <msthisari@gmail.com>
2026-02-06 16:38:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config(); 
 
 import connectDB from "./src/config/db.js";
 import app from "./src/app.js";
 
 connectDB();
 
-export default app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
