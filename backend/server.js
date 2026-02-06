902962af663f94143490ec5a336df64ec1d48bf9
Thisari Hettiarachchi <msthisari@gmail.com>
2026-02-06 16:32:20 +0530

    fix server js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,9 @@
 import dotenv from "dotenv";
-dotenv.config(); 
+dotenv.config();
 
 import connectDB from "./src/config/db.js";
 import app from "./src/app.js";
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
